b8362fb52d95f7dd926a53307af18df38f825146
fahrisengul <fahri.sengul@gmail.com>
2026-04-14 14:46:56 +0300

    Update logger.py

--- observability/logger.py
+++ observability/logger.py
@@ -1,1 +1,22 @@
+from datetime import datetime
 
+
+def now_ts():
+    return datetime.now().strftime("%H:%M:%S.%f")[:-3]
+
+
+def info(msg: str):
+    print(f"[{now_ts()}] {msg}")
+
+
+def warn(msg: str):
+    print(f"[{now_ts()}] [WARN] {msg}")
+
+
+def error(msg: str):
+    print(f"[{now_ts()}] [ERROR] {msg}")
+
+
+def debug(msg: str, enabled: bool = False):
+    if enabled:
+        print(f"[{now_ts()}] [DEBUG] {msg}")
