b4d888a46be9d7ea832ab8606be4ee80af45625e
fahrisengul <fahri.sengul@gmail.com>
2026-04-14 15:42:12 +0300

    Update prompt_builder.py

--- brain/prompt/prompt_builder.py
+++ brain/prompt/prompt_builder.py
@@ -1,1 +1,8 @@
+from ..prompt_builder import PromptBuilder, SYSTEM_PROMPT, EDUCATION_PROMPT, GENERAL_PROMPT
 
+__all__ = [
+    "PromptBuilder",
+    "SYSTEM_PROMPT",
+    "EDUCATION_PROMPT",
+    "GENERAL_PROMPT",
+]
