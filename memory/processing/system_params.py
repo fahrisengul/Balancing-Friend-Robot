a84584c7be396ba891f0104dbee43f1f2bff10a9
fahrisengul <fahri.sengul@gmail.com>
2026-04-15 18:11:12 +0300

    Update system_params.py

--- memory/processing/system_params.py
+++ memory/processing/system_params.py
@@ -1,12 +1,11 @@
 from memory.storage.db import get_connection
 
+
 class SystemParams:
 
     @staticmethod
     def get_param(key: str, default=None):
         try:
-            from memory.storage.db import get_connection
-
             with get_connection() as conn:
                 cur = conn.cursor()
                 cur.execute("""
@@ -25,9 +24,28 @@ class SystemParams:
             return default
 
     @staticmethod
+    def set_param(key: str, value: str):
+        try:
+            with get_connection() as conn:
+                cur = conn.cursor()
+
+                cur.execute("""
+                    INSERT INTO system_params (param_key, param_value)
+                    VALUES (?, ?)
+                    ON CONFLICT(param_key) DO UPDATE SET
+                        param_value = excluded.param_value,
+                        updated_at = CURRENT_TIMESTAMP
+                """, (key, value))
+
+                conn.commit()
+
+        except Exception as e:
+            print(f">>> [SET PARAM ERROR] {e}")
+
+    @staticmethod
     def get_audio_config():
         return {
-           "input_name": SystemParams.get_param("audio_input_device_name", None),
+            "input_name": SystemParams.get_param("audio_input_device_name", None),
             "input_index": SystemParams.get_param("audio_input_device_index", None),
             "output_mode": SystemParams.get_param("audio_output_mode", "system_default"),
             "output_name": SystemParams.get_param("audio_output_device_name", None),
