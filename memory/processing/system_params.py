5138e452b6ff2760780dc7918585d8d1e9b8d724
fahrisengul <fahri.sengul@gmail.com>
2026-04-15 18:46:49 +0300

    Update system_params.py

--- memory/processing/system_params.py
+++ memory/processing/system_params.py
@@ -28,7 +28,6 @@ class SystemParams:
         try:
             with get_connection() as conn:
                 cur = conn.cursor()
-
                 cur.execute("""
                     INSERT INTO system_params (param_key, param_value)
                     VALUES (?, ?)
@@ -36,9 +35,7 @@ class SystemParams:
                         param_value = excluded.param_value,
                         updated_at = CURRENT_TIMESTAMP
                 """, (key, value))
-
                 conn.commit()
-
         except Exception as e:
             print(f">>> [SET PARAM ERROR] {e}")
 
@@ -46,7 +43,6 @@ class SystemParams:
     def get_audio_config():
         return {
             "input_name": SystemParams.get_param("audio_input_device_name", None),
-            "input_index": SystemParams.get_param("audio_input_device_index", None),
             "output_mode": SystemParams.get_param("audio_output_mode", "system_default"),
             "output_name": SystemParams.get_param("audio_output_device_name", None),
         }
